12fe51a75deb82fec22db4b7df5c963d8776f771
Azizun Ussama <estro.zun@gmail.com>
2020-08-28 21:51:01 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import CharacterCard from './CharacterCard';
+import WordCard from './WordCard';
 
 const word = "Hello";
 function App() {
   return (
     <div>
+    
+      <WordCard value={word}/>
     
     </div>
   );
